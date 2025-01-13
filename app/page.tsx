911d198d91dd1a339afee6727d0060756529f367
Alec Malenfant <113561614+AlecMalenfant001@users.noreply.github.com>
2025-01-13 17:14:33 -0600

    reset-to-working-branch

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import AboutSection from "@/components/AboutSection";
 import HeroSection from "@/components/HeroSection";
+import { Amplify } from "aws-amplify";
+import config from "aws-exports.js";
+
+Amplify.configure(config);
 
 export default function Home() {
   return (
